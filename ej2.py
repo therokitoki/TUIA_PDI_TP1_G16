18d64e043201b8c600b469f381261242d0a9952a
Prompt-Sopa <agustinalsop@gmail.com>
2024-10-19 17:27:12 -0300

    sopa update

--- ej2.py
+++ ej2.py
@@ -3,71 +3,34 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def answer_line(roi):
-    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 40, 255, cv2.THRESH_BINARY)  
-    pixel_negros = np.sum(thresh == 0, axis=1)  
-    #print(pixel_negros)
-    indice = np.argmax(pixel_negros) #row con mayoria de pixeles negros
-    cv2.line(roi, (0, indice), (roi.shape[1], indice), (255, 0, 0), 1)  
-    imshow(roi)
-    return indice
-
-def opcion2(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    umbral, thresh_img = cv2.threshold(gray, 150, 255, type=cv2.THRESH_BINARY_INV)
-    #imshow(thresh_img)
-    num_labels, labels_im = cv2.connectedComponents(thresh_img)
-
-    # Dibuja las componentes conectadas
-    output = np.zeros(img.shape, dtype=np.uint8)
-    # Crear una máscara para dibujar solo las componentes que podrían ser líneas
-    for label in range(1, num_labels):
-
-        component_mask = (labels_im == label).astype("uint8") * 255
-        x, y, w, h = cv2.boundingRect(component_mask)
-
-        # Filtrar por el tamaño de las componentes, asumiendo que la línea es la más larga y delgada
-        aspect_ratio = w / h
-        if aspect_ratio > 5:  # Línea larga y delgada
-            test = img.copy()
-            z = y-14
-            crop = test[z:z+14, x:x+w,]
-            # b = 33
-            # d = 29
-            # a = 28
-            # c = 22
-            gray2 = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-            pixeles_debajo_150 = gray2 < 150
-            cantidad_pixeles = np.sum(pixeles_debajo_150)
-            umbral, thresh_img2 = cv2.threshold(gray2, thresh=230, maxval=255, type=cv2.THRESH_BINARY)
-            if cantidad_pixeles > 40:
-                valores= 'INVÁLIDO'
-            elif cantidad_pixeles == 0:
-                valores= 'NO RESPONDE'
+def letterAnswer(list_letter_box: list[np.ndarray]) -> str:
+    # b = 33
+    # d = 29
+    # a = 28
+    # c = 22
+    letter_box = cv2.cvtColor(list_letter_box[0], cv2.COLOR_BGR2GRAY)
+    pixeles_debajo_150 = letter_box < 150
+    cantidad_pixeles = np.sum(pixeles_debajo_150)
+    _, thresh_img = cv2.threshold(letter_box, thresh=230, maxval=255, type=cv2.THRESH_BINARY)
+    if cantidad_pixeles > 40:
+        letter= 'INVÁLIDO'
+    elif cantidad_pixeles == 0:
+        letter= 'NO RESPONDE'
+    else:
+        contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(letter_box, contours, contourIdx=-1, color=(0, 0, 255), thickness=1)
+        if len(hierarchy[0])==4:
+            letter= 'B'
+        elif len(hierarchy[0])==2:
+            letter= 'C'
+        else:
+            connectivity = 8
+            num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh_img, connectivity, cv2.CV_32S)
+            if stats[2][4]<15:
+                letter= 'A'
             else:
-                contours, hierarchy = cv2.findContours(thresh_img2, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-                cv2.drawContours(crop, contours, contourIdx=-1, color=(0, 0, 255), thickness=1)  # https://docs.opencv.org/3.4/d6/d6e/group__imgproc__draw.html#ga746c0625f1781f1ffc9056259103edbc
-
-                if len(hierarchy[0])==4:
-                    valores= 'B'
-                elif len(hierarchy[0])==2:
-                    valores= 'C'
-                else:
-                    print("Entreeeee")
-                    connectivity = 8
-                    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh_img2, connectivity, cv2.CV_32S)
-                    if stats[2][4]<15:
-                        valores= 'A'
-                    else:
-                        valores= 'D'
-            cv2.rectangle(img, (x, y), (x+w, y-14), (0, 255, 0), 1)
-            #imshow(crop)
-            output = cv2.bitwise_or(output, cv2.merge([component_mask, component_mask, component_mask]))
-
-    # Mostrar la imagen con la línea detectada
-    #imshow(img)
-    return(valores)
+                letter= 'D'
+    return(letter)
 
 # Defininimos función para mostrar imágenes
 def imshow(img, new_fig=True, title=None, color_img=False, blocking=True, colorbar=True, ticks=False):
@@ -88,8 +51,8 @@ def imshow(img, new_fig=True, title=None, color_img=False, blocking=True, colorb
 
 def line_detector(src : np.ndarray, th : int, for_roi = False) -> list[list[tuple]]:
     gray = cv2.cvtColor(src, cv2.IMREAD_GRAYSCALE)   # Transformamos la imagen a grayscale
-    if not for_roi:
-        imshow(gray,title='Img original gray')
+    #if not for_roi:
+       # imshow(gray,title='Img original gray')
 
     #Obtenemos los bordes mediante Canny
     edges = cv2.Canny(gray, 100, 150, apertureSize=3)
@@ -116,11 +79,8 @@ def line_detector(src : np.ndarray, th : int, for_roi = False) -> list[list[tupl
         line_list.append([(x1,y1),(x2,y2)])
         cv2.line(src_lines,(x1,y1),(x2,y2),(0,255,0),1)
 
-    if not for_roi:
-        imshow(src_lines,title='Imagen con lineas pre fix')
-
-    # El problema es que me detectó 40 líneas en lugar de 20 (por la parte superior y la inferior)
-    # Esto me puede ocasionar problemas a la hora de detectar los cuadrados
+    #if not for_roi:
+        #imshow(src_lines,title='Imagen con lineas pre fix')
 
     # Buscamos líneas cercanas entre sí y creamos las líneas 'promedio'
     final_line_list = []
@@ -185,37 +145,6 @@ def question_roi_detector(v_lines : list, h_lines : list, img : np.ndarray, show
             roi_list.append(roi)
     return (roi_list)
 
-
-# Leemos la imagen a color y la pasamos a esacala de grises
-img = cv2.imread('examen_5.png')
-#img2 = cv2.imread('examen_3.png', cv2.IMREAD_GRAYSCALE)   # Leemos imagen
-
-# Graficamos las nuevas líneas
-line_list = line_detector(img, 200)
-img_lines_new = img.copy()
-
-for i in line_list:
-    cv2.line(img_lines_new,i[0],i[1],(0,255,0),1)
-
-imshow(img_lines_new,title='Img con lineas post fix')
-
-h_lines, v_lines = line_orientation(line_list)
-
-questions = question_roi_detector(v_lines, h_lines, img, show= False)
-valores=[]
-for question in questions:
-    valores.append(opcion2(question))
-print(f'Las respuestas fueron 1:{valores[0]} 2:{valores[1]} 3:{valores[2]} 4:{valores[3]} 5:{valores[4]} 6:{valores[5]} 7:{valores[6]} 8:{valores[7]} 9:{valores[8]} 10:{valores[9]}')
-correctos = ['C','B','A','D','B','B','A','B','D','D']
-correccion= []
-for i in range(0,10):
-    if valores[i] == correctos[i]:
-        correccion.append('OK')
-    else:
-        correccion.append('MAL')
-
-print(f'Pregunta 1: {correccion[0]}\nPregunta 2: {correccion[1]}\nPregunta 3: {correccion[2]}\nPregunta 4: {correccion[3]}\nPregunta 5: {correccion[4]}\nPregunta 6: {correccion[5]}\nPregunta 7: {correccion[6]}\nPregunta 8: {correccion[7]}\nPregunta 9: {correccion[8]}\nPregunta 10: {correccion[9]}')
-
 ##################################################################################################################################################################################
 ########################################################################### TPT ESTUVO AQUÍ ######################################################################################
 ##################################################################################################################################################################################
@@ -234,91 +163,124 @@ def headerDetector(h_lines: list, img: np.ndarray, show: bool = True) -> np.ndar
     roi = img[y1+4:y2-4, x1+4:x2-4]
 
     if show:
-      imshow(roi)
+        imshow(roi)
 
     return roi
 
 
-def headerSectionsDetector(img: np.ndarray, show: bool = True) -> list:
-  sections = []
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  umbral, thresh_img = cv2.threshold(gray, 150, 255, type=cv2.THRESH_BINARY_INV)
-  #imshow(thresh_img)
-  num_labels, labels_im = cv2.connectedComponents(thresh_img)
+def letterBoxDetector(img: np.ndarray, show: bool = True, header: bool = False) -> list:
+    letter_box = []
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    umbral, thresh_img = cv2.threshold(gray, 150, 255, type=cv2.THRESH_BINARY_INV)
+    #imshow(thresh_img)
+    num_labels, labels_im = cv2.connectedComponents(thresh_img)
 
-  # Dibuja las componentes conectadas
-  output = np.zeros(img.shape, dtype=np.uint8)
-  # Crear una máscara para dibujar solo las componentes que podrían ser líneas
-  for label in range(1, num_labels):
+    # Dibuja las componentes conectadas
+    output = np.zeros(img.shape, dtype=np.uint8)
+    # Crear una máscara para dibujar solo las componentes que podrían ser líneas
+    for label in range(1, num_labels):
 
-    component_mask = (labels_im == label).astype("uint8") * 255
-    x, y, w, h = cv2.boundingRect(component_mask)
+        component_mask = (labels_im == label).astype("uint8") * 255
+        x, y, w, h = cv2.boundingRect(component_mask)
 
+        # Filtrar por el tamaño de las componentes, asumiendo que la línea es la más larga y delgada
+        aspect_ratio = w / h
+        if aspect_ratio > 5:  # Línea larga y delgada
+            if header:
+                test = img.copy()
+                z = y - 20
+                roi = test[z:z+16, x:x+w,]
+            else:
+                test = img.copy()
+                z = y-14
+                roi = test[z:z+14, x:x+w,]
+            letter_box.append(roi)
+            if show:
+                imshow(roi)
 
-    # Filtrar por el tamaño de las componentes, asumiendo que la línea es la más larga y delgada
-    aspect_ratio = w / h
-    if aspect_ratio > 5:  # Línea larga y delgada
-      test = img.copy()
-      z = y - 20
-      roi = test[z:z+16, x:x+w,]
-      sections.append(roi)
-      if show:
-        imshow(roi)
-
-  return sections
+    return letter_box
 
 
 def headerValidator(img: np.ndarray, field: str = 'name') -> bool:
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-  umbral, thresh_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    umbral, thresh_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
-  # Aplico connectedComponentsWithStats para contar componentes conectadas
-  num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh_img, connectivity=8)
+    # Aplico connectedComponentsWithStats para contar componentes conectadas
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh_img, connectivity=8)
 
-  field = field.lower()
+    field = field.lower()
 
-  # Valido parámetro field
-  if field not in ['name', 'date', 'class']:
-    raise ValueError("El parámetro field debe ser 'name', 'date' o 'class'")
+    # Valido parámetro field
+    if field not in ['name', 'date', 'class']:
+        raise ValueError("El parámetro field debe ser 'name', 'date' o 'class'")
 
-  if field == 'name':
-    # Verifico que el número de componentes conectadas sea al menos 3 (2 correspondientes al nombre y apellido y una por el fondo de la imagen)
-    if num_labels >= 3:  
-          
-      # Verifico la existencia de un espacio entre "Nombre" y "Apellido"
-      x_ant = stats[1][0]
-      for i in range(2, num_labels):
-          x, y, w, h, a = stats[i]
-          if x - x_ant > 12:
-              return True
-          x_ant = x
-    
-    return False
+    if field == 'name':
+        # Verifico que el número de componentes conectadas sea al menos 3 (2 correspondientes al nombre y apellido y una por el fondo de la imagen)
+        if num_labels >= 3:
+        # Verifico la existencia de un espacio entre "Nombre" y "Apellido"
+            x_ant = stats[1][0]
+            for i in range(2, num_labels):
+                x, y, w, h, a = stats[i]
+                if x - x_ant > 12:
+                    return True
+                x_ant = x
+            return False
 
-  elif field == 'date':
-    # Verifico que el número de componentes conectadas sea 8 (8 correspondientes a la fecha y una por el fondo de la imagen)
-    if num_labels == 9:
-      return True
-    
-    return False
+    elif field == 'date':
+        # Verifico que el número de componentes conectadas sea 8 (8 correspondientes a la fecha y una por el fondo de la imagen)
+        if num_labels == 9:
+            return True
+        return False
 
-  else:
-    # Verifico que el número de componentes conectadas sea 2 (1 correspondiente a la clase y una por el fondo de la imagen)
-    if num_labels == 2:
-      return True
-    
-    return False
-  
+    else:
+        # Verifico que el número de componentes conectadas sea 2 (1 correspondiente a la clase y una por el fondo de la imagen)
+        if num_labels == 2:
+            return True
+        return False
+
 # *******************************************************
 # *                    Implemento                       *
 # *******************************************************
-img_prb = headerDetector(h_lines, img)
-    
-nombre, fecha, clase = headerSectionsDetector(img_prb, False)
 
-nombre_ok = headerValidator(nombre, 'name')
-fecha_ok = headerValidator(fecha, 'date')
-clase_ok = headerValidator(clase, 'class')
+# Leemos la imagen a color y la pasamos a esacala de grises
+for i in range(1,6):
 
-print(f'Nombre: {nombre_ok}\nFecha: {fecha_ok}\nClase: {clase_ok}')
+    img = cv2.imread(f'examen_{i}.png')
+
+    # Graficamos las nuevas líneas
+    line_list = line_detector(img, 200)
+    img_lines_new = img.copy()
+
+    for i in line_list:
+        cv2.line(img_lines_new,i[0],i[1],(0,255,0),1)
+
+    #imshow(img_lines_new,title='Img con lineas post fix')
+
+    h_lines, v_lines = line_orientation(line_list)
+
+    questions = question_roi_detector(v_lines, h_lines, img, show= False)
+    answers=[]
+    for question in questions:
+        letter_box = letterBoxDetector(img= question, show= False, header = False)
+        answers.append(letterAnswer(letter_box))
+    print(f'Las respuestas fueron 1:{answers[0]} 2:{answers[1]} 3:{answers[2]} 4:{answers[3]} 5:{answers[4]} 6:{answers[5]} 7:{answers[6]} 8:{answers[7]} 9:{answers[8]} 10:{answers[9]}')
+    correctos = ['C','B','A','D','B','B','A','B','D','D']
+    correccion= []
+    for i in range(0,10):
+        if answers[i] == correctos[i]:
+            correccion.append('OK')
+        else:
+            correccion.append('MAL')
+
+    print(f'Pregunta 1: {correccion[0]}\nPregunta 2: {correccion[1]}\nPregunta 3: {correccion[2]}\nPregunta 4: {correccion[3]}\nPregunta 5: {correccion[4]}\nPregunta 6: {correccion[5]}\nPregunta 7: {correccion[6]}\nPregunta 8: {correccion[7]}\nPregunta 9: {correccion[8]}\nPregunta 10: {correccion[9]}')
+
+    img_prb = headerDetector(h_lines= h_lines, show= False, img= img)
+
+    nombre, fecha, clase = letterBoxDetector(img_prb, False,header=True)
+
+    nombre_ok = headerValidator(nombre, 'name')
+    fecha_ok = headerValidator(fecha, 'date')
+    clase_ok = headerValidator(clase, 'class')
+
+    print(f'Nombre: {nombre_ok}\nFecha: {fecha_ok}\nClase: {clase_ok}')
