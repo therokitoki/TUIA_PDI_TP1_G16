bec0d200e28feeb31db621c6e013bf602d52ccbb
Gonzalo Asad <gonzaloasad@hotmail.com>
2024-11-17 19:10:51 -0300

    Detección inicial letras

--- TP2/ej2.py
+++ TP2/ej2.py
@@ -25,20 +25,38 @@ plt.imshow(thresh_img, cmap='gray')
 plt.show()
 
 # 2- Componentes conectadas
-
 num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh_img, 8, cv2.CV_32S)
-
 plt.imshow(labels, cmap='gray')
 plt.show()
 
-label_filter = []
 # 3- Filtrado por área
-print(stats)
+filtered_labels = labels.copy()
+filtered_labels = filtered_labels.astype("uint8")
+
+index_list = []
 for i in range(len(stats)):
-    if stats[i][4] > 30:
-        label_filter.append(labels[i])
-plt.imshow(labels[0], cmap='gray')
+    area = stats[i][4]
+    if area < 170 and area > 30:
+        
+        # 4- Filtrado por relación de aspecto
+        ar = stats[i][3] / stats[i][2]
+        if ar >= 1.5 and ar <= 3.0:
+            index_list.append(i)
+
+mask = np.isin(filtered_labels, index_list)
+
+filtered_labels[mask] = 255
+filtered_labels[~mask] = 0
+
+plt.imshow(filtered_labels, cmap='gray')
 plt.show()
-# 4- Filtrado por relación de aspecto
 
 # 5- Componentes conectadas + BBOK + label
+img_final = img.copy()
+
+for i in index_list:
+    x, y, w, h, a = stats[i]
+    cv2.rectangle(img_final, (x, y), (x+w, y+h), (255, 0, 0), 1)
+
+plt.imshow(img_final, cmap='gray')
+plt.show()
